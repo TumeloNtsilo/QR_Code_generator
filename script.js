1a649868d29a8fb0eacb4f3daf7b09618b9d4806
Tumelo <tumelontsilo@gmail.com>
2025-10-21 20:37:40 +0200

    Project complete

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ let qrImage = document.getElementById("qrImage");
 let inputText = document.getElementById("inputText")
 
 function generateImage () {
+
+    if(inputText.value.length > 0){
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + inputText.value;
+    imgBox.classList.add("show-img");
+
+    }else {
+        inputText.classList.add("error");
+        setTimeout(() => {
+            inputText.classList.remove("error");
+        }, 1000)
+    }
 }
